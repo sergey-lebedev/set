23f31e8d54f2c008585c33bcfb9404911be26e20
Sergey Lebedev <sergey.lebedev@inbox.ru>
2012-12-07 00:12:22 +0400

    waiting event

--- game.py
+++ game.py
@@ -98,39 +98,38 @@ def interaction(cards):
     is_break = False
     field_state = {'slots': {}, 'selected': []}
     chosen_set = None
-    pygame.event.set_blocked([KEYDOWN, KEYUP, MOUSEMOTION, ACTIVEEVENT])
+    pygame.event.set_blocked([KEYDOWN, KEYUP, MOUSEMOTION, ACTIVEEVENT, MOUSEBUTTONUP])
     slots = visualize(cards, field_state)
     while not is_break:
-        time.sleep(0.2)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                exit()
-            elif event.type == MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    (x_pos, y_pos) = pygame.mouse.get_pos()
-                    slots = field_state['slots']
-                    for key in slots:
-                        slot = slots[key]
-                        (v, h) = slot['position']
-                        if v <= x_pos < v + width and h <= y_pos < h + height:
-                            if key in field_state['selected']:
-                                field_state['selected'].remove(key)
+        event = pygame.event.wait()
+        if event.type == QUIT:
+            exit()
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                (x_pos, y_pos) = pygame.mouse.get_pos()
+                slots = field_state['slots']
+                for key in slots:
+                    slot = slots[key]
+                    (v, h) = slot['position']
+                    if v <= x_pos < v + width and h <= y_pos < h + height:
+                        if key in field_state['selected']:
+                            field_state['selected'].remove(key)
+                            visualize(cards, field_state)
+                        else:
+                            if len(field_state['selected']) < set_number:
+                                field_state['selected'].append(key)
                                 visualize(cards, field_state)
-                            else:
-                                if len(field_state['selected']) < set_number:
-                                    field_state['selected'].append(key)
-                                    visualize(cards, field_state)
-                                if len(field_state['selected']) == set_number:
-                                    selected_cards = []
-                                    for number in field_state['selected']:
-                                        selected_cards.append(slots[number]['card'])
-                                    if is_set(selected_cards):
-                                        chosen_set = selected_cards
-                                        is_break = True
-                                        break
-                if event.button == 3:
-                    is_break = True
-                    break
+                            if len(field_state['selected']) == set_number:
+                                selected_cards = []
+                                for number in field_state['selected']:
+                                    selected_cards.append(slots[number]['card'])
+                                if is_set(selected_cards):
+                                    chosen_set = selected_cards
+                                    is_break = True
+                                    break
+            if event.button == 3:
+                is_break = True
+                break
     return chosen_set
 
 print 'deck init'
